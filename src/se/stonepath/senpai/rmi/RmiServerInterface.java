92e80d89fa7ca59c4bfaaff64f7257a86da4ea1a
Per-Gustaf Stenberg <per-gustaf.stenberg@stonepath.se>
2015-03-23 16:24:11 +0100

    change rebind to bind

--- src/se/stonepath/senpai/rmi/RmiServerInterface.java
+++ src/se/stonepath/senpai/rmi/RmiServerInterface.java
@@ -2,6 +2,7 @@ package se.stonepath.senpai.rmi;
 
 
 import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -14,18 +15,18 @@ import se.stonepath.senpai.log4j.SenpaiAppender;
 public interface RmiServerInterface extends Remote{
 
 	
-	public static void startServer(Logger logger, String appenderName,Remote remote) throws RemoteException, InstantiationException, IllegalAccessException, MalformedURLException{
+	public static void startServer(Logger logger, String appenderName,Remote remote) throws RemoteException, InstantiationException, IllegalAccessException, MalformedURLException, AlreadyBoundException{
 		startServer(1099,logger,appenderName, remote);
 	}
-	public static void startServer(int port, Logger logger, String appenderName, Remote remote) throws RemoteException, InstantiationException, IllegalAccessException, MalformedURLException{
+	public static void startServer(int port, Logger logger, String appenderName, Remote remote) throws RemoteException, InstantiationException, IllegalAccessException, MalformedURLException, AlreadyBoundException{
 		
 		LocateRegistry.createRegistry(port);
 
 		RmiServerInterface.bindFromSenpaiAppender(logger, appenderName, remote);
 	}
 	
-	public static void bindFromSenpaiAppender(Logger logger, String appenderName,Remote remote) throws RemoteException, MalformedURLException{
+	public static void bindFromSenpaiAppender(Logger logger, String appenderName,Remote remote) throws RemoteException, MalformedURLException, AlreadyBoundException{
 		SenpaiAppender senpaiAppender = (SenpaiAppender)logger.getAppender(appenderName);
-		Naming.rebind(senpaiAppender.getRmiUrl(), remote);
+		Naming.bind(senpaiAppender.getRmiUrl(), remote);
 	}
 }
